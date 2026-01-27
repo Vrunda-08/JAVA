e188b5548442184239d8e92ff4306402d20cc526
Vrunda-08 <136992129+Vrunda-08@users.noreply.github.com>
2026-01-27 09:22:36 +0530

    Add files via upload

---
+++ Point12.java
@@ -0,0 +1,42 @@
+ class Point12
+ {
+       int x;
+       int y;
+
+       Point12()
+       {
+          x=5;
+          y=5;
+       }
+       
+       Point12(int a, int b)
+       {
+          x=a;
+          y=b;
+       }
+
+       Point12(Point12 P)
+       {
+          x=P.x;
+          y=P.y;
+       }
+
+       void display()
+       {
+          System.out.println("Point coordination:("+ x +","+ y +")");
+       }
+
+ public static void main(String[] args)
+ {
+       System.out.println("Vrunda Vaghela (250393107016)\n");
+
+       Point12 p1 = new Point12();
+       p1.display();
+
+       Point12 p2 = new Point12(10, 20);
+       p2.display();
+
+       Point12 p3 = new Point12(p2);
+       p3.display();
+ } 
+}
