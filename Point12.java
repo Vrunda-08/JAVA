c79ee896aa391e832e140d3b5c840eeb191c08e0
Vrunda-08 <136992129+Vrunda-08@users.noreply.github.com>
2026-01-27 09:24:45 +0530

    Point12.java

--- Point12.java
+++ Point12.java
@@ -1,33 +1,27 @@
- class Point12
- {
+ class Point12{
        int x;
        int y;
 
-       Point12()
-       {
+       Point12(){
           x=5;
           y=5;
        }
        
-       Point12(int a, int b)
-       {
+       Point12(int a, int b){
           x=a;
           y=b;
        }
 
-       Point12(Point12 P)
-       {
+       Point12(Point12 P){
           x=P.x;
           y=P.y;
        }
 
-       void display()
-       {
+       void display(){
           System.out.println("Point coordination:("+ x +","+ y +")");
        }
 
- public static void main(String[] args)
- {
+ public static void main(String[] args){
        System.out.println("Vrunda Vaghela (250393107016)\n");
 
        Point12 p1 = new Point12();
