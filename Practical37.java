07d9e9a2195e322b25ed92424b7f7ad0526512d0
Vrunda-08 <136992129+Vrunda-08@users.noreply.github.com>
2026-05-06 12:29:59 +0530

    Add files via upload

---
+++ Practical37.java
@@ -0,0 +1,38 @@
+class BookNotAvailableException extends Exception {
+    public BookNotAvailableException(String message) {
+        super(message);
+    }
+}
+
+class Library {
+    int availableBooks;
+
+    public Library(int availableBooks) {
+        this.availableBooks = availableBooks;
+    }
+
+    public void issueBook(int count) throws BookNotAvailableException {
+        if (count <= availableBooks) {
+            availableBooks -= count;
+            System.out.println("Book issued successfully");
+            System.out.println("Available books: " + availableBooks);
+        } else {
+            throw new BookNotAvailableException("Requested books not available");
+        }
+    }
+}
+
+public class Practical37 {
+    public static void main(String[] args) {
+        System.out.println("Vrunda Vaghela (250393107016)\n");
+        Library lib = new Library(3);
+
+        try {
+            lib.issueBook(2);
+            lib.issueBook(2);
+        }
+        catch (BookNotAvailableException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+}
