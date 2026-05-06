2d9c55b7f8768de4915c824ecf9a978f27a722aa
Vrunda-08 <136992129+Vrunda-08@users.noreply.github.com>
2026-05-06 12:31:13 +0530

    Add files via upload

---
+++ Practical40.java
@@ -0,0 +1,41 @@
+class Table {
+    synchronized void printTable(int n) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+        System.out.println();
+    }
+}
+
+class T1 extends Thread {
+    Table t;
+    T1(Table t) {
+        this.t = t;
+    }
+    public void run() {
+        t.printTable(5);
+    }
+}
+
+class T2 extends Thread {
+    Table t;
+    T2(Table t) {
+        this.t = t;
+    }
+    public void run() {
+        t.printTable(7);
+    }
+}
+
+public class Practical40 {
+    public static void main(String[] args) {
+        System.out.println("Vrunda Vaghela (250393107016)\n");
+        Table obj = new Table();
+
+        T1 t1 = new T1(obj);
+        T2 t2 = new T2(obj);
+
+        t1.start();
+        t2.start();
+    }
+}
